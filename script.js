cc03eeca63746e80f30f5df877d0fea1153bb0bd
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-08-04 20:29:44 +0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function change1() {
+  document.getElementById('imageprofil').src = 'assets/image/profilxlogo/fotoprofil.png';
+}
+function change2() {
+  document.getElementById('imageprofil').src = 'assets/image/profilxlogo/fotoprofil2.png';
+}
+function change3() {
+  document.getElementById('imageprofil').src = 'assets/image/profilxlogo/fotoprofil3.png';
+}
